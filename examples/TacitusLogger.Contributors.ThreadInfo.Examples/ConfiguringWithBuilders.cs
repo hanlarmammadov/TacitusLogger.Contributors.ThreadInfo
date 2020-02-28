8fd294bcc8f40b91ad3b73a29ab9bf5f7319b859
Khanlar Mammadov <khanlar@mammadov.pro>
2020-02-29 00:47:31 +0400

    Examples project added

---
+++ examples/TacitusLogger.Contributors.ThreadInfo.Examples/ConfiguringWithBuilders.cs
@@ -0,0 +1,40 @@
+﻿using TacitusLogger.Builders;
+
+namespace TacitusLogger.Contributors.ThreadInfo.Examples
+{
+    class ConfiguringWithBuilders
+    {
+        public void Adding_Thread_Info_Contributor_To_The_Logger()
+        {
+            Logger logger = LoggerBuilder.Logger()
+                                         .Contributors()
+                                             .ThreadInfo()
+                                         .BuildContributors()
+                                         .ForAllLogs()
+                                             .Console().Add()
+                                         .BuildLogger();
+        }
+        public void Explicitly_Specifying_Name_And_Status_Of_Thread_Info_Contributor()
+        {
+            Logger logger = LoggerBuilder.Logger()
+                                         .Contributors()
+                                             .ThreadInfo(true, "Thread")
+                                         .BuildContributors()
+                                         .ForAllLogs()
+                                             .Console().Add()
+                                         .BuildLogger();
+        }
+        public void Explicitly_Specifying_Mutable_Status_Of_Thread_Info_Contributor()
+        {
+            MutableSetting<bool> status = Setting<bool>.From.Variable(true);
+
+            Logger logger = LoggerBuilder.Logger()
+                                         .Contributors()
+                                             .ThreadInfo(status)
+                                         .BuildContributors()
+                                         .ForAllLogs()
+                                             .Console().Add()
+                                         .BuildLogger();
+        }
+    }
+}
